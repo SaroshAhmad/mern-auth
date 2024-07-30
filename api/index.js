0f9fd43044a7b7a1bd24efe44f2a9bebff073625
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-07-30 22:51:07 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,41 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const mongoURI = process.env.MONGO;
+// console.log(`MongoDB URI: ${mongoURI}`); // For debugging; remove in production
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+})
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+
+
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// mongoose.connect(process.env.MONGO).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// const app = express();
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
